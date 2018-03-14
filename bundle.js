584803a42f8e1f280c57d3d6abd29203f5719028
Thomas Hansen <thansen1694@gmail.com>
2018-03-13 21:05:32 -0400

    Start pathfinding

--- bundle.js
+++ bundle.js
@@ -2879,6 +2879,41 @@ class Ghost extends __WEBPACK_IMPORTED_MODULE_1__movable_object__["a" /* default
      }
   }
 
+  goHome(board, quackMan, grid, squareWidth, squareHeight){
+    // const grids = this.calculateMatrixPos(this.x, this.y, this.width, this.height);
+    // const startX = grids.gridX;
+    // const startY = grids.gridY - 1;
+    // const endX = 7;
+    // const endY = 9; // make these the default positions
+    // while(!(Math.abs(this.x - (squareWidth * startX)) <=5 ) && !(Math.abs(this.y - (squareHeight * startY)) <=5 )){
+    //   let path = this.findShortestPath(startX, startY, endX, endY, grid);
+    //   const direction = path[0];
+    //     switch (direction) {
+    //       case 'up':
+    //         this.direction = [0, -1];
+    //         break;
+    //       case 'right':
+    //         this.direction = [1, 0];
+    //         break;
+    //       case 'down':
+    //         this.direction = [0, 1];
+    //         break;
+    //       case 'left':
+    //         this.direction = [-1, 0];
+    //         break;
+    //       default:
+    //     }
+    //   this.x += this.direction[0] * this.speed;
+    //   this.y += this.direction[1] * this.speed;
+    //   board.draw();
+  //}
+    // const quacks = quackMan.calculateMatrixPos(quackMan.x, quackMan.y, quackMan.width, quackMan.height);
+    // const endX = quacks.gridX;
+    // const endY = quacks.gridY - 1;
+
+
+  }
+
   flicker(){
     this.vulnerable = true;
     window.setTimeout(() => {
@@ -5979,13 +6014,15 @@ class QuackMan extends __WEBPACK_IMPORTED_MODULE_2__movable_object__["a" /* defa
       }, 200);
       board.score += 200;
       ghost.eaten = true;
+      ghost.goHome(board, this, board.grid, this.squareWidth, this.squareHeight);
       // ghost.vulnerable = true;
       this.ctx.font = '12px PressStart';
       this.ctx.fillText("200", this.x, this.y - 5);
       window.setTimeout(() => {
         ghost.eaten = false;
+        console.log('just got home');
         // ghost.vulnerable = true;
-      }, 2000);
+      }, 4000);
       ghost.vulnerable = false;
     }
   }
@@ -24930,7 +24967,7 @@ X.X.o....X......X.X
 X.X.XX.XXXXX.XX.X.X
 X.X.X........XX.X.X
 X...X.XXXX X.XX...X
-XXX.X.Xb   X....XXX
+XXX.X.XbpicX....XXX
    .X.XXXXXX.XX.
 XXX.X........XX.XXX
 X.o...XXXXXX.XX...X
@@ -32431,7 +32468,7 @@ class Blinky extends __WEBPACK_IMPORTED_MODULE_1__ghost__["a" /* default */] {
       ghostImg.src = `./assets/blinky.png`;
       this.ctx.drawImage(ghostImg, this.x, this.y, this.width, this.height);
       this.speed = .8;
-      // this.speed = 1.25;
+      this.speed = 1.25;
     } else {
       if(this.eaten){
         this.speed = 3;
@@ -32447,100 +32484,80 @@ class Blinky extends __WEBPACK_IMPORTED_MODULE_1__ghost__["a" /* default */] {
     }
   }
 
-  move(board, grid, squareWidth, squareHeight, quackMan){
-    const grids = this.calculateMatrixPos(this.x, this.y, this.width, this.height);
-    const startX = grids.gridX;
-    const startY = grids.gridY - 1;
-    const quacks = quackMan.calculateMatrixPos(quackMan.x, quackMan.y, quackMan.width, quackMan.height);
-    const endX = quacks.gridX;
-    const endY = quacks.gridY - 1;
+  // move(board, grid, squareWidth, squareHeight, quackMan){
+  //   const grids = this.calculateMatrixPos(this.x, this.y, this.width, this.height);
+  //   const startX = grids.gridX;
+  //   const startY = grids.gridY - 1;
+  //   const quacks = quackMan.calculateMatrixPos(quackMan.x, quackMan.y, quackMan.width, quackMan.height);
+  //   const endX = quacks.gridX;
+  //   const endY = quacks.gridY - 1;
+  //
+  //   let path = this.findShortestPath(startX, startY, endX, endY, grid);
+  //   console.log(path);
+  //
+  //   const direction = path[0];
+  //     switch (direction) {
+  //       case 'up':
+  //         this.direction = [0, -1];
+  //         break;
+  //       case 'right':
+  //         this.direction = [1, 0];
+  //         break;
+  //       case 'down':
+  //         this.direction = [0, 1];
+  //         break;
+  //       case 'left':
+  //         this.direction = [-1, 0];
+  //         break;
+  //       default:
+  //     }
+  //   this.x += this.direction[0] * this.speed;
+  //   this.y += this.direction[1] * this.speed;
+  //
+  //   this.wrap(this.x, this.y);
+  //   this.quackCollision(board, quackMan);
+  //   this.draw();
+  // }
 
-    let path = this.findShortestPath(startX, startY, endX, endY, grid);
-    console.log(path);
+  // getNextDirection(grid, squareWidth, squareHeight){
+  //   // debugger
+  //   this.path.slice(1);
+  //   this.changeGhostDirection(grid, squareWidth, squareHeight);
+  // }
 
-    const direction = path[0];
-      switch (direction) {
-        case 'up':
-          this.direction = [0, -1];
-          // this.nextDirection = [0, -1];
-          break;
-        case 'right':
-          this.direction = [1, 0];
-          // this.nextDirection = [1, 0];
-          break;
-        case 'down':
-          this.direction = [0, 1];
-          // this.nextDirection = [0, 1];
-          break;
-        case 'left':
-          this.direction = [-1, 0];
-          // this.nextDirection = [-1, 0];
-          break;
-        default:
-      }
-      // this.changeGhostDirection(grid, squareWidth, squareHeight);
-    // const startingX = this.x;
-    // const startingY = this.y;
-    this.x += this.direction[0] * this.speed;
-    this.y += this.direction[1] * this.speed;
-
-    // let finalPos = this.calculateMatrixPos(startingX, startingY, squareWidth, squareHeight);
-    // const offsetX = (squareWidth - this.width) / 2;
-    // const offsetY = (squareHeight - this.height) / 2;
-
-
-    // if(this.wallCollision(grid)){
-    //   this.x = finalPos.x + offsetX;
-    //   this.y = finalPos.y + offsetY;
-      // debugger
-      // this.findShortestPath(startX, startY, endX, endY, grid);
-      // this.getNextDirection(grid, squareWidth, squareHeight);
-    // }
-
-    this.wrap(this.x, this.y);
-    this.quackCollision(board, quackMan);
-    this.draw();
-  }
-
-  getNextDirection(grid, squareWidth, squareHeight){
-    // debugger
-    this.path.slice(1);
-    this.changeGhostDirection(grid, squareWidth, squareHeight);
-  }
-
-  changeGhostDirection(grid, squareWidth, squareHeight){
-    // debugger
-    if(!this.nextDirection) return;
-
-    const center = this.calculateCurrentCenter();
-    const ghostX = center.x;
-    const ghostY = center.y;
-
-    const currentLocation = this.calculateMatrixPos(ghostX, ghostY, squareWidth, squareHeight);
-    const currentGridX = currentLocation.gridX;
-    const currentGridY = currentLocation.gridY;
-
-    const nextCenter = this.calculateNextCenter(currentGridX, currentGridY, squareWidth, squareHeight);
-    const nextCenterX = nextCenter.x;
-    const nextCenterY = nextCenter.y;
-
-    const nextX = currentGridX + this.nextDirection[0];
-    const nextY = currentGridY + this.nextDirection[1];
-    if(grid.length >= nextX &&
-        grid[0].length >= nextY){
-          const nextCell = grid[nextY][nextX];
-          if(!(nextCell instanceof __WEBPACK_IMPORTED_MODULE_0__wall__["a" /* default */])){
-              if(this.notInverseDirection()) {
-                if (this.smoothMovement(ghostX, ghostY, nextCenterX, nextCenterY)) {
-                  return;
-                }
-                this.x = currentGridX * squareWidth + (squareWidth - this.width) / 2;
-                this.y = currentGridY * squareHeight + (squareHeight - this.height) / 2;
-              }
-            this.direction = this.nextDirection;
-        }
-    }
-  }
+  // changeGhostDirection(grid, squareWidth, squareHeight){
+  //   // debugger
+  //   if(!this.nextDirection) return;
+  //
+  //   const center = this.calculateCurrentCenter();
+  //   const ghostX = center.x;
+  //   const ghostY = center.y;
+  //
+  //   const currentLocation = this.calculateMatrixPos(ghostX, ghostY, squareWidth, squareHeight);
+  //   const currentGridX = currentLocation.gridX;
+  //   const currentGridY = currentLocation.gridY;
+  //
+  //   const nextCenter = this.calculateNextCenter(currentGridX, currentGridY, squareWidth, squareHeight);
+  //   const nextCenterX = nextCenter.x;
+  //   const nextCenterY = nextCenter.y;
+  //
+  //   const nextX = currentGridX + this.nextDirection[0];
+  //   const nextY = currentGridY + this.nextDirection[1];
+  //   if(grid.length >= nextX &&
+  //       grid[0].length >= nextY){
+  //         const nextCell = grid[nextY][nextX];
+  //         if(!(nextCell instanceof Wall)){
+  //             if(this.notInverseDirection()) {
+  //               if (this.smoothMovement(ghostX, ghostY, nextCenterX, nextCenterY)) {
+  //                 return;
+  //               }
+  //               this.x = currentGridX * squareWidth + (squareWidth - this.width) / 2;
+  //               this.y = currentGridY * squareHeight + (squareHeight - this.height) / 2;
+  //             }
+  //           this.direction = this.nextDirection;
+  //       }
+  //   }
+  // }
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (Blinky);
